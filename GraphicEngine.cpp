c7a8674d3381e77ddf0068eea8a4bba488f583eb
leo <lgogi19@freeuni.edu.ge>
2021-08-23 04:13:26 +0400

    adding textures in working process

--- GraphicEngine.cpp
+++ GraphicEngine.cpp
@@ -8,7 +8,6 @@ float Render::angleBetweenVectors(sf::Vector2f vec1, sf::Vector2f vec2) {
 }
 
 
-
 float Render::vectorLength(sf::Vector2f vec) {
     return sqrt(pow(vec.x, 2.f) + pow(vec.y, 2.f));
 }
@@ -18,6 +17,27 @@ float Render::cosineOfAngleBetweenVectors(sf::Vector2f vec1, sf::Vector2f vec2) 
     return ((vec1.x * vec2.x) + (vec1.y * vec2.y)) / (vectorLength(vec1) * vectorLength(vec2));
 }
 
+
+void Render::setPosition(float x, float y) {
+    Render::playerX = x;
+    Render::playerY = y;
+}
+
+
+void Render::setMap(std::string map, int mapWdth, int mapHght) {
+    this->map = map;
+    this->mapWdth = mapWdth;
+    this->mapHght = mapHght;
+}
+
+
+sf::Vector2f Render::norm(sf::Vector2f vec) {
+    float vecLen = Render::vectorLength(vec);
+    return sf::Vector2f(vec.x / vecLen, vec.y / vecLen);
+}
+
+
+
 void Render::start() {
 
     sf::ContextSettings settings;
@@ -26,12 +46,20 @@ void Render::start() {
     sf::RenderWindow window(sf::VideoMode(Render::screenWdth,Render::screenHght), "2.5D",sf::Style::Default,settings);
     std::vector<sf::RectangleShape> walls;
 
+    std::vector<sf::Sprite> texturedWalls;
 
+    //512 x 512
+    sf::Texture txt;
+    txt.loadFromFile("texture.png");
+
+    
     const float delay = 0.020f;
     sf::Clock clock;
     float chrono = 0;
     float maxDepth = (sqrt(Render::mapHght * Render::mapHght + Render::mapWdth * Render::mapWdth));
     Render::playerDir = sf::Vector2f(cosf(playerAngle), sinf(playerAngle));
+
+    float angleDiff = Render::fieldOfView / (2.f *  (float)Render::screenWdth);
 
     while (window.isOpen())
     {
@@ -41,22 +69,27 @@ void Render::start() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        chrono += clock.getElapsedTime().asSeconds();
+        float deltaT = clock.getElapsedTime().asSeconds();
+        chrono += deltaT;
         clock.restart();
-        Render::playerDir = sf::Vector2f(cosf(playerAngle), sinf(playerAngle));
 
 
         if (chrono >= delay) {
             
             walls.clear();
+            texturedWalls.clear();
             chrono = 0;
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+               // Render::playerAngle += 2.f * deltaT;
                 Render::playerAngle += 0.1f;
+                Render::playerDir = sf::Vector2f(cosf(playerAngle), sinf(playerAngle));
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                //Render::playerAngle -= 2.f *  deltaT;
                 Render::playerAngle -= 0.1f;
+                Render::playerDir = sf::Vector2f(cosf(playerAngle), sinf(playerAngle));
                
             }
             
@@ -79,7 +112,11 @@ void Render::start() {
             for (int i = 0; i < Render::screenWdth; i++) {
                 float rayAngle = (Render::playerAngle - (Render::fieldOfView / 2.f) + ((float)i / (float)Render::screenWdth) * Render::fieldOfView);
                 float distanceToWall = 0;
+
+                float pos = 0;
+
                 bool hitwall = false;
+                bool isEdge = false;
                 
                 float dirY = sinf(rayAngle);
                 float dirX = cosf(rayAngle);
@@ -130,50 +167,102 @@ void Render::start() {
                         yChangeDistance += unitStepSizeY;    
                     }
 
-                    int testX = (int)(Render::playerX + dirX * distanceToWall);
-                    int testY = (int)(Render::playerY + dirY * distanceToWall);
 
-
+ 
                     if (currBoxX >= 0 && currBoxX < Render::mapWdth && currBoxY >= 0 && currBoxY < Render::mapHght) {
                         if (Render::map[(currBoxY * Render::mapWdth) + currBoxX] == '1') {
                             hitwall = true;
+
+
+                            float testX = (Render::playerX + dirX * distanceToWall);
+                            float testY = (Render::playerY + dirY * distanceToWall);
+
+                            int blockX = (int)(Render::playerX + dirX * distanceToWall);
+                            int blockY = (int)(Render::playerY + dirY * distanceToWall);
+
+                          /*  std::vector<std::pair<float ,float >> edges;*/
+                            
+                           /* for (int tx = 0; tx < 2; tx++) {
+                                for (int ty = 0; ty > -2; ty--) {
+                                    int edgeX = (int)testX + tx;
+                                    int edgeY = (int)testY + ty;
+                                    sf::Vector2f rayBack = sf::Vector2f(edgeX - Render::playerX,edgeY - Render::playerY);
+                                    float vecLength = Render::vectorLength(rayBack);
+                                    float angleBetween = Render::angleBetweenVectors(rayBack,sf::Vector2f(dirX, dirY));
+                                    edges.push_back(std::make_pair(vecLength, angleBetween));
+                                }
+                            }
+
+                            std::sort(edges.begin(), edges.end(), [](const std::pair<float, float>& left, const std::pair<float, float>& right) {return left.first < right.first; });
+
+                            if (edges[0].second <= angleDiff || edges[1].second <= angleDiff)isEdge = true;*/
+
+
+                            sf::Vector2f blockCenter((float)blockX + 0.5f, (float)blockY + 0.5f);
+
+                            sf::Vector2f forAngle(testX - blockCenter.x, testY - blockCenter.y);
+
+
+                            float testAngle = atan(forAngle.y / forAngle.x);
+
+                            if ((testAngle >= Render::pi / 4.f && testAngle <= (3.f / 4.f) * Render::pi) || (testAngle < -Render::pi / 4.f && testAngle > -(3.f / 4.f) * Render::pi)) {
+                                pos = testX - (float)blockX;
+                            }
+                            else {
+                                pos = testY - (float)blockY;
+                            }
+
+                            
                         }
                     }
+
+                  
                 }
 
                 float fishEyeAngleFix = Render::cosineOfAngleBetweenVectors(Render::playerDir, sf::Vector2f(dirX, dirY));
+
 
                 distanceToWall *= fishEyeAngleFix;
 
                 float ceilling = (float)(screenHght / 2.f) - (screenHght / (float)distanceToWall);
                 float floor = screenHght - ceilling;
-                sf::RectangleShape wall;
+               /* sf::RectangleShape wall;
                 wall.setSize(sf::Vector2f(1, floor - ceilling));
-                wall.setPosition(sf::Vector2f(i, ceilling));
-                wall.setFillColor(sf::Color::Yellow);
-                walls.push_back(wall);
+                wall.setPosition(sf::Vector2f(i, ceilling));*/
+
+                sf::Sprite wallSprite;
+                wallSprite.setTexture(txt);
+                wallSprite.setTextureRect(sf::IntRect(pos * 512, 0, 1, 512));
+                wallSprite.setScale(sf::Vector2f(1, (floor - ceilling) / 512.f));
+                wallSprite.setPosition(sf::Vector2f(i, ceilling));
+
+                int color = 256 - (distanceToWall / (float)Render::mapWdth) * 256;
+                wallSprite.setColor(sf::Color(color, color, color));
+                
+                
+
+                texturedWalls.push_back(wallSprite);
+               
+               /* if (isEdge) {
+                    wall.setFillColor(sf::Color::Black);
+                }
+                else {
+                    int color = 256 - (distanceToWall / (float)Render::mapWdth) * 256;
+                    wall.setFillColor(sf::Color(color, color, color));
+                }*/
+               /* int color = 256 - (distanceToWall / (float)Render::mapWdth) * 256;
+                walls.push_back(wall);*/
             }
 
         }
 
         window.clear();
-        for (int i = 0; i < walls.size(); i++)window.draw(walls[i]);
+        for (int i = 0; i < texturedWalls.size(); i++)window.draw(texturedWalls[i]);
         window.display();
     }
     return;
 }
 
 
-void Render::setPosition(float x, float y) {
-    Render::playerX = x;
-    Render::playerY = y;
-}
 
-
-
-void Render::setMap(std::string map, int mapWdth, int mapHght) {
-    this->map = map;
-    this->mapWdth = mapWdth;
-    this->mapHght = mapHght;
-}
 
--- GraphicEngine.h
+++ GraphicEngine.h
@@ -4,6 +4,7 @@
 #include<iostream>
 #include"math.h"
 
+
 class Render {
 private:
 	sf::Vector2f playerDir;
@@ -16,12 +17,13 @@ private:
 	int mapWdth;
 	int mapHght;
 
-	const float pi = 3.14159;
+	const float pi = 3.14159f;
 
 	float playerX = 0.f;
 	float playerY = 0.f;
 	float playerAngle = 0.f;
 	float fieldOfView = pi / 3.f;
+
 
 public:
 
@@ -32,7 +34,7 @@ public:
 	float angleBetweenVectors(sf::Vector2f vec1, sf::Vector2f vec2);
 	float vectorLength(sf::Vector2f vec);
 	float cosineOfAngleBetweenVectors(sf::Vector2f vec1, sf::Vector2f vec2);
-
+	sf::Vector2f norm(sf::Vector2f vec);
 };
 
 
